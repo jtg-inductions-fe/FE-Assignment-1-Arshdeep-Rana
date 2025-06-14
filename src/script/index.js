54888e4dcdab302e651bf11a6d28924b61efcbda
ArshdeepRana-FE <arshdeep.rana@joshtechnologygroup.com>
2025-06-14 11:15:49 +0530

    AR_RU_02 :  header : saving changes

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,8 @@
 import '../styles/main.scss';
+
+document.querySelector('.header__hamburger').addEventListener('click', () => {
+    let siderbar = document.querySelector('.header__sidebar');
+
+    if (siderbar.style.display == 'none') siderbar.style.display = 'flex';
+    else siderbar.style.display = 'none';
+});
