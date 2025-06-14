3e80b6cd2ec08dff211853ac7d74a66d844feab8
ArshdeepRana-FE <arshdeep.rana@joshtechnologygroup.com>
2025-06-14 17:52:20 +0530

    AR_RU_02 : header : header with sidebar completed

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,6 @@
 import '../styles/main.scss';
 
+<<<<<<< HEAD
 
 
 // Grab references to elements
@@ -16,6 +17,18 @@ const toggleSidebar = () => {
 
     if (isSidebarOpen) {
         sidebar.style.display = 'flex';
+=======
+//variables go here
+let siderbar = document.querySelector('.header__sidebar');
+let hamburger = document.querySelector('.header__hamburger');
+let blurOverlay = document.querySelector('.blur-overlay');
+
+//event listeners go here
+document.querySelector('.header__hamburger').addEventListener('click', () => {
+    //if the sidebar is not opened then open it or else close it
+    if (siderbar.style.display == 'none') {
+        siderbar.style.display = 'flex';
+>>>>>>> 601ba52 (AR_RU_02 : header : header with sidebar completed)
         blurOverlay.style.backdropFilter = 'blur(10px)';
         hamburger.style.transform = 'rotate(90deg)';
     } else {
