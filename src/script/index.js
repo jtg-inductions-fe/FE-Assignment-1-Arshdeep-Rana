cab76238682e9fdd98e6f75a7c45c0fa3b17427d
ArshdeepRana-FE <arshdeep.rana@joshtechnologygroup.com>
2025-06-14 17:52:20 +0530

    AR_RU_02 : header : header with sidebar completed

--- src/script/index.js
+++ src/script/index.js
@@ -1,10 +1,13 @@
 import '../styles/main.scss';
 
+//variables go here
 let siderbar = document.querySelector('.header__sidebar');
 let hamburger = document.querySelector('.header__hamburger');
 let blurOverlay = document.querySelector('.blur-overlay');
 
+//event listeners go here
 document.querySelector('.header__hamburger').addEventListener('click', () => {
+    //if the sidebar is not opened then open it or else close it
     if (siderbar.style.display == 'none') {
         siderbar.style.display = 'flex';
         blurOverlay.style.backdropFilter = 'blur(10px)';
