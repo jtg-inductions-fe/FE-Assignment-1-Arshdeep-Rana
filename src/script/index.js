c3f64e5cc935872545fadc0be5d1cad104c348d9
ArshdeepRana-FE <arshdeep.rana@joshtechnologygroup.com>
2025-06-14 16:16:46 +0530

    AR_RU_02 : header : header with sidebar completed

--- src/script/index.js
+++ src/script/index.js
@@ -5,11 +5,13 @@ let hamburger = document.querySelector('.header__hamburger');
 let blurOverlay = document.querySelector('.blur-overlay');
 
 document.querySelector('.header__hamburger').addEventListener('click', () => {
-    if (siderbar.style.display == 'none' && hamburger.style.display != 'none') {
+    if (siderbar.style.display == 'none') {
         siderbar.style.display = 'flex';
         blurOverlay.style.backdropFilter = 'blur(10px)';
+        hamburger.style.transform = 'rotate(90deg)';
     } else {
         siderbar.style.display = 'none';
         blurOverlay.style.backdropFilter = '';
+        hamburger.style.transform = 'rotate(180deg)';
     }
 });
