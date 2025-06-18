bc9d83fa6af1365c51a199602433962bd91c314d
ArshdeepRana-FE <arshdeep.rana@joshtechnologygroup.com>
2025-06-18 13:14:35 +0530

    AR_RU_02 : Header : Re styled header, added animation to sidebar

--- src/script/index.js
+++ src/script/index.js
@@ -39,22 +39,6 @@ function toggleSidebar() {
 }
 
 hamburger.addEventListener('click', toggleSidebar);
-
-document.addEventListener('keydown', (e) => {
-    let signup = document.querySelector('.auth__signup');
-    let logo = document.querySelector('.header__logo');
-    if (e.key == 'Tab') {
-        //  e.preventDefault();
-
-        if (document.activeElement == signup) {
-            logo.focus();
-        }
-
-        if (isSidebarOpen) {
-            let signup = document.querySelector('.signup');
-            if (document.activeElement == signup) {
-                hamburger.focus();
-            }
-        }
-    }
+overlay.addEventListener('click', () => {
+    if (isSidebarOpen) toggleSidebar();
 });
