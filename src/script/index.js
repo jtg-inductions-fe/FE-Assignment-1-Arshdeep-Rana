2b70552c03cf0638819f247759ea8549afed2879
ArshdeepRana-FE <arshdeep.rana@joshtechnologygroup.com>
2025-06-21 12:01:21 +0530

    AR_RU_02 : Header : Created some more variables, and components

--- src/script/index.js
+++ src/script/index.js
@@ -1,15 +1,37 @@
 import '../styles/main.scss';
 
-const hamburger = document.querySelector('.header__hamburger');
-const navLinks = document.querySelector('.header__nav-links');
+const hamburger = document.querySelector('.js-header__hamburger');
+const navLinksContainer = document.querySelector('.js-nav-links__container');
+const navLinks = document.querySelector('.js-header__nav-links');
 
-let isSidebarOpen = false;
-
+/**
+ * Function name : toggleSidebar
+ * Purpose : When clicked on the hamburger button on phone or tablet screen, this function toggles classes
+ * on nav-links on phones and on nav-links-container on tablets.
+ */
 function toggleSidebar() {
-    isSidebarOpen = !isSidebarOpen;
-    navLinks.classList.toggle('header__nav-links--open', isSidebarOpen);
+    if (hamburger && navLinksContainer && navLinks) {
+        /**
+         * Variable name : navLinkIsRelative
+         * Note : nav-link is relative on tablet screen.
+         * Purpose : To figure out what screen, hamburger was clicked.
+         */
+        const navLinksIsRelative =
+            window.getComputedStyle(navLinks).getPropertyValue('position') ===
+            'relative';
+        document.body.classList.toggle('no-scroll');
 
-    const iconEl = hamburger.firstElementChild;
+        if (navLinksIsRelative)
+            navLinksContainer.classList.toggle('nav-links__container--open');
+        else navLinks.classList.toggle('header__nav-links--open');
+    }
+
+    /**
+     * Variable name : iconEl
+     * Purpose : To switch hamburger and close icons
+     */
+    const iconEl = document.querySelector('.js-sidebar__icon');
+
     if (iconEl) {
         iconEl.classList.toggle('icon-Vector');
         iconEl.classList.toggle('icon-cross');
